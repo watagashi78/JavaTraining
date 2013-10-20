870fa5d4417eb127180cefbe3505a7c3177aa7be
hase <lonvalleriver@gmail.com>
2013-10-20 14:06:50 +0900

    2013/10/20 add

---
+++ ch02/src/ex02_04/Ex02_04.java
@@ -0,0 +1,13 @@
+package ex02_04;
+
+public class Ex02_04 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Q. 練習問題 2.3に対する答えについて考えてください。識別番号フィールドはfinalとすべきですか。");
+		System.out.println("A. ");
+	}
+
+}
