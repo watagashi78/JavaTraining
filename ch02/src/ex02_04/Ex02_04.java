1f59d3eb4cd96335f9cb23535caef982d87df755
hase <lonvalleriver@gmail.com>
2013-10-22 20:51:39 +0900

    2013/10/22 add

--- ch02/src/ex02_04/Ex02_04.java
+++ ch02/src/ex02_04/Ex02_04.java
@@ -7,7 +7,7 @@ public class Ex02_04 {
 	 */
 	public static void main(String[] args) {
 		System.out.println("Q. 練習問題 2.3に対する答えについて考えてください。識別番号フィールドはfinalとすべきですか。");
-		System.out.println("A. ");
+		System.out.println("A. 次の識別番号を持つ必要があり、インスタンスが生成されるたびに変化するためfinalとすべきではない");
 	}
 
 }
