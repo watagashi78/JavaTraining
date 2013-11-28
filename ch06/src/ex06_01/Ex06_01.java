e4c156aaff53e85799a169ffdc6a0a73b93e9898
hase <lonvalleriver@gmail.com>
2013-11-29 06:44:14 +0900

    2013/11/29 add

---
+++ ch06/src/ex06_01/Ex06_01.java
@@ -0,0 +1,25 @@
+package ex06_01;
+
+public class Ex06_01 {
+
+	enum Days {
+		MONDAY,
+		TUESDAY,
+		WEDNESDAY,
+		THURSDAY,
+		FRIDAY,
+		SATURDAY,
+		SUNDAY
+	}
+
+	enum TrafficRight {
+		GREEN,
+		YELLOW,
+		RED
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Q. 週の曜日と信号機の色に対する単純なenumを定義しなさい");
+	}
+
+}
---
+++ ch06/src/ex06_03/Verbose.java
@@ -0,0 +1,13 @@
+package ex06_03;
+
+public interface Verbose {
+	enum Verbosity {
+		SILENT,
+		TERSE,
+		NORMAL,
+		VERBOSE
+	}
+
+	void setVerbosity(int level);
+	int getVerbosity();
+}
