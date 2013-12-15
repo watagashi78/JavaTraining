08c5c91f1bf0e8e5c3668ad72010ea67bdca716b
hase <lonvalleriver@gmail.com>
2013-12-15 15:33:10 +0900

    2013/12/15 add

---
+++ ch07/src/ex07_01/EscapeWorld.java
@@ -0,0 +1,8 @@
+package ex07_01;
+
+public class EscapeWorld {
+	public static void main(String[] args) {
+		// Hello,World
+		System.out.println("\u0048\u0065\u006C\u006C\u006F\u002C\u0057\u006F\u0072\u006C\u0064");
+	}
+}
---
+++ ch07/src/ex07_02/Literal.java
@@ -0,0 +1,43 @@
+package ex07_02;
+
+public class Literal {
+	public static void main(String[] args) {
+		/*
+		int a1 = null;			// ×
+		int a2 = true;			// ×
+		int a3 = "test";		// ×
+		int a4 = 035;			// ○
+		int a5 = 3.2f;			// ×
+		int a6 = '\n';			// ○
+
+		double b1 = null;		// ×
+		double b2 = true;		// ×
+		double b3 = "test";		// ×
+		double b4 = 035;		// ○
+		double b5 = 3.2f;		// ○
+		double b6 = '\n';		// ○
+
+		String c1 = null;		// ○
+		String c2 = true;		// ×
+		String c3 = "test";		// ○
+		String c4 = 035;		// ×
+		String c5 = 3.2f;		// ×
+		String c6 = '\n';		// ×
+
+		boolean d1 = null;		// ×
+		boolean d2 = true;		// ○
+		boolean d3 = "test";	// ×
+		boolean d4 = 035;		// ×
+		boolean d5 = 3.2f;		// ×
+		boolean d6 = '\n';		// ×
+
+		char e1 = null;			// ×
+		char e2 = true;			// ×
+		char e3 = "test";		// ×
+		char e4 = 035;			// ○
+		char e5 = 3.2f;			// ×
+		char e6 = '\n';			// ○
+		*/
+	}
+
+}
