61ec775a6e59c57f54ccffa5596ee6da0d695d0a
Takashi@Desktop <Takashi@Takashi-PC>
2014-07-09 06:51:30 +0900

    2014/07/09 add

---
+++ JPL/ch17/ex17_03/Resource.java
@@ -0,0 +1,6 @@
+package ex17_03;
+
+interface Resource {
+	void use(Object key, Object... args);
+	void release();
+}
---
+++ JPL/ch17/ex17_03/ResourceImpl.java
@@ -0,0 +1,36 @@
+package ex17_03;
+
+import java.lang.ref.WeakReference;
+
+public class ResourceImpl implements Resource {
+	WeakReference<Object> keyRef;
+	boolean needsRelease = false;
+
+	ResourceImpl(Object key) {
+		keyRef = new WeakReference<Object>(key);
+
+		// .. 外部リソースの設定
+
+		needsRelease = true;
+	}
+
+	@Override
+	public void use(Object key, Object... args) {
+		if (!keyRef.get().equals(key)) {
+			throw new IllegalArgumentException("wrong key");
+		}
+
+		// ... リソースの利用 ...
+
+	}
+
+	@Override
+	public void release() {
+		if (needsRelease) {
+			needsRelease = false;
+
+			// .. リソースの解放 ...
+
+		}
+	}
+}
